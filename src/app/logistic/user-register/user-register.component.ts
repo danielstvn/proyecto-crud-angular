bc33aca50b23927978ab8b48d485aa23c91ba042
danielstvn <danielstvn@outlook.es>
2022-07-06 18:02:51 -0500

    cambios en el frontend

--- src/app/logistic/user-register/user-register.component.ts
+++ src/app/logistic/user-register/user-register.component.ts
@@ -17,25 +17,55 @@ export class UserRegisterComponent implements OnInit {
 
   formRegisterClient !: FormGroup;
 
+  dniClient !: String ;
+
+  public isCollapsed = true;
+
+  public isSubmited !:boolean ;
+
 
   constructor(private clienteService: RegisterClientService,private router: Router, public formBuilder:FormBuilder) { 
 
     this.formRegisterClient = this.formBuilder.group({
       name: ['', [Validators.required]],
       last_name: ['', [Validators.required]],
-      email: ['', [Validators.required],[Validators.pattern("^[a-z0-9._%+-]+@[a-z0-9.-]+\\.[a-z]{2,4}$")]],
-      dni: ['', [Validators.required]],
-      phone: ['', [Validators.required],[Validators.pattern("[0-9]{10}")]],
+      email: ['', [Validators.required, Validators.pattern("^[a-z0-9._%+-]+@[a-z0-9.-]+\\.[a-z]{2,4}$")]],
+      dni: ['', [Validators.required, Validators.pattern("[0-9]{10}")]],
+      phone: ['', [Validators.required ,Validators.pattern("[0-9]{10}")]],
       location: ['', [Validators.required]],
       country: ['', [Validators.required]],
       province: ['', [Validators.required]],
       city: ['', [Validators.required]],
+      postal_code: ['', [Validators.required ,Validators.pattern("[0-9]{6}")]],
     });
+
+    
+      this.isSubmited = false;
+    
+    
+
   }
 
   addClient(){
     this.clienteService.addClient(this.client).subscribe(dato=>{
       console.log(dato);
+      this.dniClient = this.client.dni;
+      this.isSubmited = true;
+
+      console.log(this.isSubmited);
+
+    },error=>console.log(error));
+    
+  }
+
+  editClient(){
+    this.clienteService.addClient(this.client).subscribe(dato=>{
+      console.log(dato);
+      this.dniClient = this.client.dni;
+      this.isSubmited = true;
+
+      console.log(this.isSubmited);
+
     },error=>console.log(error));
     
   }
@@ -46,7 +76,14 @@ export class UserRegisterComponent implements OnInit {
   onSubmit(): void {
    //console.log(this.client);
    this.addClient();
+
   }
+
+  onUpdate(): void {
+    //console.log(this.client);
+    this.editClient();
+ 
+   }
 
   
 
